819782630a8f5150704c0df8f34bbef2ec921f59
iain8 <ib0487@gmail.com>
2017-04-16 16:04:03 +0200

    more refactoring, arguments

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # rust-hugo-content-maker
-simple thing for generating hugo content from media
+
+simple thing for generating hugo content from images
+
+# Usage
+
+Pass it:
+  - a directory of photos
+  - a place to output content
+  - the longest side of the "base" image (usually the largest) in px
+
+A folder structure is expected of the following format
+
+  /<category>/<size>px/yourimage.jpg 
+  
+  e.g. /outdoors/3200px/dog.jpg
+
+The content maker will collect all sizes for a given image name, tagged with the given category (as
+a taxonomy of "photos"). The image under the folder with the name matching the third argument will
+be read to check the dimensions and these will also be recorded in the content entry, useful for 
+scaling etc. or serving images of various dimensions.
--- src/main.rs
+++ src/main.rs
@@ -11,34 +11,37 @@ use image::GenericImage;
 use chrono::prelude::*;
 use walkdir::WalkDir;
 
-// expected path /category/size/image.jpg
+// TODO: doc comments
 fn main() {
-  let args: Vec<String> = env::args().collect();
+    let args: Vec<String> = env::args().collect();
 
-  if args.len() == 2 {
-    println!("importing files from path {:?}", &args[1]);
+    if args.len() == 3 {
+        println!("importing files from path {:?}", &args[1]);
 
-    let mut category = String::from("uncategorised");
-    let mut width = String::from("0px");
+        let mut category = String::from("uncategorised");
+        let mut width = String::from("0px");
+        let mut order = 0;
 
-    for entry in WalkDir::new(&args[1]) {
-      let entry = entry.unwrap();
-      let file_name = entry.file_name().to_str().unwrap();
+        for entry in WalkDir::new(&args[1]) {
+            let entry = entry.unwrap();
+            let file_name = entry.file_name().to_str().unwrap();
 
-      // remove file extension
-      let title = file_name.split(".").collect::<Vec<_>>()[0];
+            // remove file extension
+            let title = file_name.split(".").collect::<Vec<_>>()[0];
 
-      if entry.file_type().is_dir() {
-        if file_name.contains("px") {
-          width = file_name.to_owned();
-        } else {
-          category = file_name.to_owned();
-        }
-      } else if width == "3200px" {
-        let dimensions = image::open(&entry.path()).unwrap().dimensions();
-        let date: DateTime<UTC> = UTC::now();
-        let data = format!(
-      "+++
+            if entry.file_type().is_dir() {
+                if file_name.contains("px") {
+                    width = file_name.to_owned();
+                } else {
+                    order = 0;
+                    category = file_name.to_owned();
+                }
+            } else if width == "3200px" {
+                order += 1;
+
+                let dimensions = image::open(&entry.path()).unwrap().dimensions();
+                let date: DateTime<UTC> = UTC::now();
+                let data = format!("+++
 title = \"{}\"
 image = \"{}\"
 width = {}
@@ -46,68 +49,65 @@ height = {}
 date = \"{}\"
 draft = false
 photos = [\"{}\"]
+order = {}
 +++\n",
-          &title,
-          &file_name,
-          &dimensions.0,
-          &dimensions.1,
-          &date.to_rfc3339(),
-          &category
-        );
+                  &title,
+                  &file_name,
+                  &dimensions.0,
+                  &dimensions.1,
+                  &date.to_rfc3339(),
+                  &category,
+                  &order);
 
-        save_entry(&category, &title, &data);
+                save_entry(&category, &title, &data, &args[2]);
 
-        println!("created entry {:?}", &title);
-      }
+                println!("created entry {:?}", &title);
+            }
+        }
+
+        // TODO: error handling?
+
+        println!("DONE");
+    } else {
+        help();
     }
-
-    // TODO: error handling?
-
-    // TODO: output path as arg?
-
-    println!("DONE");
-  } else {
-    help();
-  }
 }
 
 fn help() {
-  println!("usage: uploader <path to files>");
+    println!("usage: uploader <path to files> <path to output>");
 }
 
-fn save_entry(category: &str, title: &str, data: &str) {
-  // TODO: base path?
+fn save_entry(category: &str, title: &str, data: &str, base_path: &str) {
+    let category_dir = format!("{}/{}", &base_path, &category);
 
-  let category_dir = format!("./photos/{}", &category);
-
-  // create category dir if it doesn't exist already
-  match fs::metadata(&category_dir) {
-    Err(msg) => {
-      // TODO: handle this!
-      fs::create_dir(&category_dir);
-    },
-    Ok(metadata) => {
-      if !metadata.is_dir() {
-        panic!("unable to create dir {}", &category_dir);
-      }
+    // create category dir if it doesn't exist already
+    match fs::metadata(&category_dir) {
+        Err(msg) => {
+            // TODO: handle this!
+            fs::create_dir(&category_dir);
+        }
+        Ok(metadata) => {
+            if !metadata.is_dir() {
+                panic!("unable to create dir {}", &category_dir);
+            }
+        }
     }
-  }
 
-  let filename = format!("./photos/{}/{}.md", &category, &title);
-  let path = Path::new(&filename);
-  let display = path.display();
+    let filename = format!("./photos/{}/{}.md", &category, &title);
+    let path = Path::new(&filename);
+    let display = path.display();
 
-  // TODO: don't create file if exists already - (in future update!)
+    // TODO: don't create file if exists already - (in future update!)
 
-  let mut file = match fs::File::create(&path) {
-    Err(msg) => panic!("unable to create file {}: {}", display, msg.description()),
-    Ok(file) => file,
-  };
+    let mut file = match fs::File::create(&path) {
+        Err(msg) => panic!("unable to create file {}: {}", display, msg.description()),
+        Ok(file) => file,
+    };
 
-  match file.write_all(data.as_bytes()) {
-    Err(msg) => panic!("unable to write to file {}: {}", display, msg.description()),
-    Ok(file) => file,
-  }
+    match file.write_all(data.as_bytes()) {
+        Err(msg) => panic!("unable to write to file {}: {}", display, msg.description()),
+        Ok(file) => file,
+    }
 
-  // TODO: return value
+    // TODO: return value
 }
